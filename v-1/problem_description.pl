4c1fc56eb4c76fefb87d21967bc7554d1f7481b7
FrancescoPerrone <francescoperr@gmail.com>
2014-07-15 11:54:13 +0100

    I added predicates for the definition of Actions Available.

--- v-1/problem_description.pl
+++ v-1/problem_description.pl
@@ -3,7 +3,9 @@
  * -------------------------------
  * This module implements a representation of Hal and Carla dilemma.
 
- * @author Francesco Perrone */
+ * @author Francesco Perrone
+ *
+ */
 
 %% attribute's values
 fbool(2).
@@ -16,6 +18,15 @@ bool(0).
 agent(carla).
 agent(hal).
 
+%% Valid state definition
+%% state(+s(I, M, A, W))
+state(Ag, s(I, M, A, W)):-
+    bool(I),
+    fbool(M),
+    fbool(A),
+    bool(W),
+    agent(Ag).
+
 %% Agent's attributes
 
 %% NOTE: forse devi mettere valid_state(s(I,_,_,_))
@@ -24,7 +35,7 @@ agent(hal).
 %% gli stati dove I = 1.
 has_insulin(Ag, I):-
     agent(Ag),
-    insulin(_, I),
+    insulin(_, I).
 insulin(s(I, _, _, _), I):-
     bool(I).
 
@@ -32,12 +43,13 @@ has_money(Ag, M):-
     agent(Ag),
     money(_, M).
 money(s(_, M, _, _), M):-
-    valid_state(s(_, M, _, _)),
     fbool(M).
 
+%% evaluate if this version with the addition of
+%% state(Ag, State) is better then the others. 
 is_alive(Ag, A):-
     agent(Ag),
-    alive(_,A).
+    alive(_, A).
 alive(s(_, _, A, _), A):-
     fbool(A).
 
@@ -46,13 +58,37 @@ world(W):-
 w_attribute(s(_, _, _, W), W):-
     bool(W).
 
-%% Agent's state(+State)
-state(State):-
-    valid_state(State).
+%% Actions
+lose(Ag, State):-
+    State = s(1, _, A, _),
+    is_alive(Ag, A), member(A, [1, 2]).
+%% this is a cleaner version that uses the previous
+%% predicates, but is is better?
+lose1(Ag):-
+    has_insulin(Ag, 1),
+    is_alive(Ag, A), member(A, [1, 2]).
 
-%% valid_state(+State:a state)
-valid_state(s(I, M, A, W)):-
-    bool(I),
-    fbool(M),
-    fbool(A),
-    bool(W).
+%% Other actions
+compensate(Ag1, Ag2):-
+    has_insulin(Ag1, 1),
+    has_money(Ag1, M), member(M, [1,2]),
+    is_alive(Ag1, A), member(A, [1,2]),
+    agent(Ag2),
+    not(Ag1 = Ag2).
+
+take(Ag1, Ag2):-
+    has_insulin(Ag1, 0),
+    is_alive(Ag1, 1),
+    has_insulin(Ag2, 1),
+    not(Ag1 = Ag2).
+
+buy(Ag, State):-
+    State = s(0, M, A, 1),
+    has_money(Ag, M), member(M, [1,2]),
+    is_alive(Ag, A), member(A, [1,2]).
+
+do_nothing(Ag, State):-
+    state(Ag, State).
+do_nothing(Ag, State):-
+    agent(Ag),
+    State = s(_, _, 0, _).
