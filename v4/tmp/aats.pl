10cbf9dd860dcfb417e361ae84477dcbd5dd5a75
FrancescoPerrone <francescoperr@gmail.com>
2014-08-11 09:56:11 +0100

    aats updated

--- v4/tmp/aats.pl
+++ v4/tmp/aats.pl
@@ -6,26 +6,28 @@
 :- use_module(q).     % ℚ (the set of valid states)
 :- use_module(qi).    % qi designed initial state ∣ (qi ∈ ℚ)
 :- use_module(gc).    % ℭ (the grand coalition)
-% :- use_module(ac).    % Ac finite set of action for each agent ∈ Ag
 :- use_module(jag).   % Jag joint action
 :- use_module(acpre). % ρ (action precondition function)
+% :- use_module(ac).  % Ac finite set of action for each agent ∈ Ag
 
-
-% Define a language below.
 
 % ℒ (the language)
+% -----------------------------------------------------------
 
+% agents
+agent(hal).
+agent(carla).
+
+% agent's attributes
 attributes([i,m,a,s]).
 
+% attribute's domanin
 domain(i,[1,0]).
 domain(m, [2,1,0]).
 domain(a, [2,1,0]).
 domain(s, [1,0]).
 
-% Ag (set of agents)
-agent(hal).
-agent(carla).
-
-% Ac (set of action for each agent ∈ Ag)
+% agent's actions
 actions(carla, [buy, compensate, doNothing, lose, take]).
 actions(hal, [buy, compensate, doNothing, lose, take]).
+
