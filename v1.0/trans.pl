d554c54c9045d918fd1ebb5db819c44c7c90c78f
FrancescoPerrone <francescoperr@gmail.com>
2026-04-12 21:42:18 +0100

    remove stale freedomH initial state added for earnH

--- v1.0/trans.pl
+++ v1.0/trans.pl
@@ -12,11 +12,6 @@ initial_state(Init):-
     Init = [1,1,1,0,_,1],
     state(Init).
 
-% Hal lacks money but has insulin (for freedomH)
-initial_state(Init):-
-    Init = [1,0,1,1,_,1],
-    state(Init).
-
 % Carla lacks money; Hal has everything and can compensate (for freedomC)
 initial_state(Init):-
     Init = [1,1,1,1,0,1],
